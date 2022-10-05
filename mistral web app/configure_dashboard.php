f5b58df137c1248b9dbd0fbcc626a65ee8cc44c7
Bharathi-18 <84948997+Bharathi-18@users.noreply.github.com>
2022-10-05 19:14:53 +0530

    Dashboard title

--- mistral web app/configure_dashboard.php
+++ mistral web app/configure_dashboard.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>Configure Dashboard</title>
+        <title>Dashboard Configuration</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
@@ -9,6 +9,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
         
         <style>
+
             .card{
                 margin-top:5%;
                 margin-left: 50px;
