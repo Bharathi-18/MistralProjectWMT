e38c01a39aada86f51774461e4714742a3ad9887
Bharathi-18 <me.bharathic@gmail.com>
2022-10-17 10:57:27 +0530

    edit dashboard - 1

--- mistral web app/edit_dashboard.php
+++ mistral web app/edit_dashboard.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link href = "https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel = "stylesheet">
 	<script src = "https://code.jquery.com/jquery-1.10.2.js"></script>
 	<script src ="https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
@@ -27,57 +29,87 @@
 		function dispVal()
 		{
 			var i = document.getElementById("output").value;
-            window.location = "edit_dashboard.php?ord="+i+"?dbn=";
-
+            var name = document.querySelector("#dashboardname").textContent;
+            // console.log(name);
+            window.location = "updateDash.php?ord="+i+"&dbn="+name;
 		}
 	</script>
     <style>
-
-    .names{
-        text-align: center;
-        font-size:32px;
-        color:#1F2937;
-        font-weight: bold;
-      }
-
+        .names{
+            text-align: center;
+            font-size:32px;
+            color:#1F2937;
+            font-weight: bold;
+        }
+        .btn_cls{
+            float: right;
+            font-size: 20px;
+            background-color:#1F2937;
+            color:whitesmoke;
+        }
+        ::placeholder{
+            color: whitesmoke;
+        }
+        #output{
+            display:none;
+        }
+        .btn1{
+            font-size: 6px;
+            width:200px;
+            border:none;
+            height:40px;
+            background-color:#1F2937;
+            color:whitesmoke;
+            text-align:center;
+            border-radius:5px;
+        }
+        .btn1:hover{
+            color: #1F2937;
+            background-color: whitesmoke;
+        }
+        .editDash{
+            font-size: 16px;
+        }
+        .content{
+            margin-left: 38%;
+            align-items: center;
+        }
     </style>
     </head>
     <body>
-    <?php 
-        session_start();
-        $host = "localhost";
-        $user = "root";
-        $pass = "";
-        $db = $_SESSION['user'];
-        // $db = "wmt1";
-        $conn = new mysqli($host,$user,$pass,$db);
-        $query = "SELECT * FROM dbnames where names='".$_GET['dbn']."'";
-        
-    ?>
-    <div class="names">
-        <?php echo $_GET['dbn']; ?>
-    </div>
-
-    <?php
-
-        $dashBoard = $conn->query($query);
-        $dashBoardRes = mysqli_fetch_assoc($dashBoard);
-        $tableName = $dashBoardRes['id'];
-        $val = "SELECT * from ".$tableName;
-        $exec = $conn->query($val);
-        $oldLis = [];
-        $i = 0;
-        $res = "<div id=\"reorder\">";
-        while($str = mysqli_fetch_assoc($exec))
-        {
-            $oldLis[$i++] = $str['locSen'];
-            $res = $res."<div id=\"div".($i-1)."\" class=\"editDash\"><h2>".$str['locSen']."</h2></div>";
-        }
-        $res .= "</div><div class=\"order\"><input id=\"output\" type=\"text\" value=\"\"></div>";
-        echo $res;
-    ?>
-    
-    <button onclick="dispVal()">update</button>
-
+        <?php 
+            session_start();
+            $host = "localhost";
+            $user = "root";
+            $pass = "";
+            $db = $_SESSION['user'];
+            $conn = new mysqli($host,$user,$pass,$db);
+            $query = "SELECT * FROM dbnames where names='".$_GET['dbn']."'";
+        ?>
+        <div class="names">
+           <?php echo " <h3 id=\"dashboardname\">".$_GET['dbn']."</h3>"; ?>
+        </div>
+        <br><br>
+        <div class="content">
+            <?php
+                $dashBoard = $conn->query($query);
+                $dashBoardRes = mysqli_fetch_assoc($dashBoard);
+                $tableName = $dashBoardRes['id'];
+                $val = "SELECT * from ".$tableName;
+                $exec = $conn->query($val);
+                $oldLis = [];
+                $i = 0;
+                $res = "<div id=\"reorder\">";
+                while($str = mysqli_fetch_assoc($exec))
+                {
+                    $oldLis[$i++] = $str['locSen'];
+                    $res = $res."<div id=\"div".($i-1)."\" class=\"editDash\"><h5>".$str['locSen']."</h5></div>";
+                }
+                $res .= "</div><div class=\"order\"><input id=\"output\" type=\"text\" value=\"\"></div>";
+                echo $res;
+            ?>
+            <br><br>
+            <button onclick="dispVal()" class="btn1" id="config" style="font-size:17px">Update</button>
+        </div>
     </body>
 </html>
--- mistral web app/updateDash.php
+++ mistral web app/updateDash.php
@@ -1,14 +1,52 @@
 <?php
 
+use function PHPSTORM_META\type;
+
     session_start();
     $host = "localhost";
     $user = "root";
     $pass = "";
     $db = $_SESSION['user'];
     $conn = new mysqli($host,$user,$pass,$db);
+    // echo $_GET['dbn'];
     $query = "SELECT * FROM dbnames where names='".$_GET['dbn']."'";
+    $ord = $_GET['ord'];
     $dashBoard = $conn->query($query);
     $dashBoardRes = mysqli_fetch_assoc($dashBoard);
     $tableName = $dashBoardRes['id'];
+    $val = "SELECT * from ".$tableName;
+    $exec = $conn->query($val);
+    $oldLis = [];
+    $i = 0;
+    while($str = mysqli_fetch_assoc($exec))
+    {
+        $oldLis[$i++] = $str['locSen'];
+    }
 
+    $ordLis = [];
+    $ordRes = "";
+    $indexVal = 0;
+    for($temp = 0;$temp<strlen($ord);$temp++)
+    {
+        if($ord[$temp]!=',')
+        {
+            $ordRes.=$ord[$temp];
+        }
+        else
+        {
+            $ordLis[$indexVal++] = (int)$ordRes;
+            $ordRes = "";
+        }
+    }
+    $ordLis[$indexVal++] = (int)$ordRes;
+    $query = "truncate table ".$tableName;
+    $del = $conn->prepare($query);
+    $del->execute();
+    for($tempvar = 0; $tempvar<$i ;$tempvar++)
+    {
+        $newIns = $oldLis[$ordLis[$tempvar]];
+        $insertDash = "INSERT INTO ".$tableName." VALUES('".$newIns."')";
+        $dashInserted = $conn->prepare($insertDash);
+        $dashInserted->execute();
+    }
 ?>
